e5c3287ae56c1d25a8e2b37ae4849962afdde147
lx071 <onlyfor_ever@163.com>
2024-03-30 19:47:38 +0800

    update

--- C/C_omp/C_1/omp.cpp
+++ C/C_omp/C_1/omp.cpp
@@ -5,16 +5,32 @@
 // OpenMp parallel
 // parallerl 指令用于创建一个并行域，告诉编译器这段代码我要并行执行
 
-void Func()
+void Func1()
 {
 	int length = 5;
 
-	// num_thread表示创建几个线程执行，如果不指定就创建最大线程数
-	// omp_get_thread_num()打印当前线程id
-#pragma omp parallel num_threads(3)
+	// num_thread 表示创建几个线程执行，如果不指定就创建最大线程数
+	// omp_get_thread_num() 打印当前线程id
+	#pragma omp parallel num_threads(3)
 	{
 		for (int i = 0; i < length; i++)
 			std::cout << i << ": " << omp_get_thread_num() << std::endl;
+	}
+	#pragma omp parallel
+	{
+		std::cout << "Func1:" << omp_get_thread_num() << std::endl;;
+	}
+}
+
+void Func2()
+{
+	int length = 5;
+	// 将 OMP_NUM_THREADS 变量设置为 5
+	// 也可以设置环境变量 export OMP_NUM_THREADS=5 
+	omp_set_num_threads(5); 
+	#pragma omp parallel
+	{
+		std::cout << "Func2:" << omp_get_thread_num() << std::endl;;
 	}
 }
 
@@ -22,7 +38,8 @@ int main()
 {
 	auto start = std::chrono::high_resolution_clock::now();
 
-	Func();
+	Func1();
+	Func2();
 
 	auto end = std::chrono::high_resolution_clock::now();
 
--- C/C_omp/README.md
+++ C/C_omp/README.md
@@ -1,3 +1,4 @@
 C_0: support OpenMp  
 C_1: OpenMp parallel  
 C_2: OpenMp for  
+C_3: OpenMp lock  
